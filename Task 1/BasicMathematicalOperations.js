6959d242596777c621012bb949d4a31a7360caf9
Neha Nehra <neha_nehra@epam.com>
2017-10-31 14:33:31 +0530

    Corrected indentation

--- Task 1/BasicMathematicalOperations.js
+++ Task 1/BasicMathematicalOperations.js
@@ -1,20 +1,20 @@
 function basicOp(operation, value1, value2){
-var result;
-switch(operation) {
-    case '+':
-      result  = (value1 + value2);
-      break;
-    case '-':
-      result  = (value1 - value2);
-      break;
-    case '*':
-      result  = (value1 * value2);
-      break;
-    case '/':
-      result  = (value1 / value2);
-      break;
-    default:
-    result = 0;
-    }
+    var result;
+    switch(operation) {
+        case '+':
+          result  = (value1 + value2);
+          break;
+        case '-':
+          result  = (value1 - value2);
+          break;
+        case '*':
+          result  = (value1 * value2);
+          break;
+        case '/':
+          result  = (value1 / value2);
+          break;
+        default:
+            result = 0;
+        }
     return result;
 }
